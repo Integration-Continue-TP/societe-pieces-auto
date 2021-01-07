439aa2ea1fb50cdcf4574ee63453d4f10d5e05e5
Aurélien Riche <contact@aurelienriche.com>
2021-01-07 14:17:29 +0100

    ci

---
+++ frontend/ci.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+yarn
+yarn test
+yarn build --prod
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
 function Login() {
     return (
         <div className="Login">
             <h1>Login</h1>
+            <Button variant="primary">Primary</Button>{' '}
         </div>
     );
 }
